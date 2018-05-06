9a1f08b1d2238475a39df91367fdc5ca3acb907a
Pedro Pereira <j.pedro.diniz1@gmail.com>
2018-05-06 07:41:43 +0100

    landing page
    cenas varias só na landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,8 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <style>
             html, body {
                 background-image: url("image/homepage.jpg");
@@ -19,7 +21,7 @@
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
-                margin: 0;
+                margin: 5;
             }
 
             .full-height {
@@ -39,7 +41,7 @@
             .top-right {
                 position: absolute;
                 right: 10px;
-                top: 18px;
+                top: 25px;
             }
 
             .content {
@@ -52,7 +54,7 @@
 
             .links > a {
                 color: #636b6f;
-                padding: 0 25px;
+                padding: 0 35px;
                 font-size: 12px;
                 font-weight: 600;
                 letter-spacing: .1rem;
@@ -61,13 +63,110 @@
             }
 
             .m-b-md {
-                margin-bottom: 30px;
+                margin-bottom: 40px;
             }
         </style>
     </head>
     <body>
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel"  style="padding: 0%; border: 0%;">
+            <div class="container" style="margin-left: 7%; margin-bottom: 10px; margin-top: 10px;max-width:1200px">
+                <a class="navbar-brand nav nav-justified" href="{{ url('/') }}">
+                    <!--<img src="{{ asset('image/IMMERJ_LOGO_NAVBAR.png') }}" alt="IMMERJ LOGO" width="125" height="35">-->
+                    <!--<img src="{{ config('app.img') }}" alt="IMMERJ LOGO" width="125" height="35"></>-->
+                    <!--{{ config('app.name', 'IMMERJ') }}-->
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+                    <div class=" links">
+                    <a href="{{ url('/home') }}">Home</a>
+                    <a href="{{ url('/home') }}">Profiles</a>
+                    <a class="navbar-brand nav nav-justified" href="{{ url('/') }}">
+                    Connect
+                    </a> 
+        </div>   
+                 <!--   <div class="flex-center position-ref full-height">-->
+           
+            <div class=" links">
+                <a class="navbar-brand nav nav-justified" href="{{ url('/') }}">
+                        <img src="{{ asset('image/IMMERJ_LOGO_NAVBAR.png') }}" alt="IMMERJ LOGO" width="125" height="35">
+                </a>
+
+            
+            <a class="navbar-brand nav nav-justified" href="{{ url('/') }}">
+                    Helping Others
+            </a>
+        </div>
+        @if (Route::has('login'))
+                <div class=" links">
+            </a>
+                    @auth
+                        <a href="{{ url('/home') }}">About</a>
+                        
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                    
+                </div>
+            @endif
+        <!--</div>-->
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @auth
+                        
+                        <div class=" links">
+                        <a href="{{ url('/home') }}" role="button"   v-pre>
+        </div>
+                                    {{ Auth::user()->name }}
+                                </a>
+                        <!--<li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    
+                                
+                                <!--<a href="{{ url('/home') }}">Home</a>-->
+                                
+                                <!--<a class="dropdown-item" href="{{ url('/home') }}">
+                                        {{ url('/home') }}
+                                </a>-->
+                                <!--<a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>-->
+        </div>
+                    @else
+                   <!-- <li class="{{ Request::is('login') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    <li class="{{ Request::is('register') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>-->
+                    @endauth
+                       
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+           <!-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -77,20 +176,48 @@
                         <a href="{{ route('register') }}">Register</a>
                     @endauth
                 </div>
-            @endif
+            @endif-->
 
             <div class="content">
                 <div class="title m-b-md">
-                {{ config('app.name', 'IMMERJ') }}
+                    <a style="color: white; font-weight: bold; font-size: 55px;" >FIND THE PERFECT CARER!</a>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            
+
+            <form method="POST" action="">
+                        @csrf
+                <div class="form-group row center">
+                            
+
+                            <div class="col-md-2">
+                                <input id="doubt_area" type="text" class="form-control" name="doubt_area" value="Doubt Area" required>
+
+                                @if ($errors->has('country'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('country') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        
+                            <div class="col-md-2">
+                                <input id="country" type="text" class="form-control" name="doubt_area" value="Country" required>
+
+                                @if ($errors->has('country'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('country') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-2 offset-md-2">
+                                <button type="submit" class="btn btn-primary">
+                                    <!--{{ __('search') }}-->Search!
+                                </button>
+                            </div>
+                        </div>
+                </form>
             </div>
         </div>
     </body>
