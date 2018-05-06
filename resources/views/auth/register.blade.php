7f511ef66c285b2d090b402e4cdf25e31a08948d
rmntavares <ricardo@ricardomnt.com>
2018-05-06 10:42:58 +0100

    register_bug

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -96,7 +96,7 @@
                         <div class="form-group row">
                             <label for="role" class="col-md-4 col-form-label text-md-right">Birth date</label>
                             <div class="col-md-6">
-                                <input id="date" class="form-control" type="date" name="datebirth">
+                                <input id="date" class="form-control" type="date" name="birth">
                             </div>
                         </div>
 
@@ -115,7 +115,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>
+                            <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Address') }}</label>   
 
                             <div class="col-md-6">
                                 <input id="address" type="text" class="form-control{{ $errors->has('address') ? ' is-invalid' : '' }}" name="address" value="{{ old('address') }}" required>
