5428f5014d515a2137bae3eeb024180a99ef3ec3
rmntavares <ricardo@ricardomnt.com>
2018-05-06 02:15:41 +0100

    a

---
+++ externo/include/footer.php
@@ -0,0 +1,5 @@
+<footer class="text-center">
+      <div class="row text-center">  
+              <p class="text"><!--Copyright (c)--> 2018, IMMERJ TEAM</p>
+      </div>
+    </footer>
---
+++ externo/include/header.php
@@ -0,0 +1,27 @@
+
+<!--### Bootstrap ###-->
+<!-- CSS -->
+ <link rel="stylesheet" href="include/css/bootstrap.css"> 
+ <link rel="stylesheet" href="include/css/bootstrap.min.css"> 
+<!-- JS -->
+
+
+<!-- ### META TAGS  ### -->
+
+<meta charset="UTF-8">
+<meta name="description" content="IMMERJ">
+<meta name="keywords" content="Imigrantes, Emigrantes, Gestão, Refugiados, Inegração, CRM, IMMERJ, Portal de Integração Apoio e ">
+<meta name="author" content="Pedro Pereira, Ricardo Tavares, Margarida Alves, Mariana Res, Iolanda Correia, Joana Fontes">
+
+<!-- REFRESH RATE-->
+<meta http-equiv="refresh" content="3000000">
+
+<!--FAVICON -->
+<link href="/dashboard/images/favicon.png" rel="icon" type="image/png" />
+
+<!-- VIEWPORT SCALE -->
+<!-- NÃO HÁ SHRINK TO FIT-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+
+
