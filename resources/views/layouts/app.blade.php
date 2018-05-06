cf14e2c4265d49ab53261e7586f23ac3598a9e82
Pedro Pereira <j.pedro.diniz1@gmail.com>
2018-05-06 09:44:13 +0100

    app layout updated navbar
    bajoras

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,17 +16,71 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <!--<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+            html, body {
+                
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 5;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 25px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 35px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 40px;
+            }
+        </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel"  style="padding: 0%; border: 0%;">
-            <div class="container">
+
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel"  style="padding: 0%; border: 0%;">
+            <div class="container" style="margin-left: 7%; margin-bottom: 10px; margin-top: 10px;max-width:1200px">
                 <a class="navbar-brand nav nav-justified" href="{{ url('/') }}">
-                    <img src="{{ asset('image/IMMERJ_LOGO_NAVBAR.png') }}" alt="IMMERJ LOGO" width="125" height="35">
+                    <!--<img src="{{ asset('image/IMMERJ_LOGO_NAVBAR.png') }}" alt="IMMERJ LOGO" width="125" height="35">-->
                     <!--<img src="{{ config('app.img') }}" alt="IMMERJ LOGO" width="125" height="35"></>-->
                     <!--{{ config('app.name', 'IMMERJ') }}-->
                 </a>
@@ -39,11 +93,126 @@
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
+                    <div class=" links">
+                    <a class="{{ Request::is('home') ? 'home' : '' }}" href="{{ url('/home') }}">Home</a>
+                    <a class="{{ Request::is('profile') ? 'active' : '' }}" href="{{ url('/profile') }}">Profiles</a>
+                    <a class="{{ Request::is('connect') ? 'active' : '' }} navbar-brand nav nav-justified" href="{{ url('/') }}">
+                    Connect
+                    </a> 
+        </div>   
+                 <!--   <div class="flex-center position-ref full-height">-->
+           
+            <div class=" links">
+                <a class="{{ Request::is('connect') ? 'active' : '' }} navbar-brand nav nav-justified" href="{{ url('/') }}">
+                        <img src="{{ asset('image/IMMERJ_LOGO_NAVBAR.png') }}" alt="IMMERJ LOGO" width="125" height="35">
+                </a>
 
+            
+            <a class="{{ Request::is('connect') ? 'active' : '' }} navbar-brand nav nav-justified" href="{{ url('/') }}">
+                    Helping Others
+            </a>
+        </div>
+        @if (Route::has('login'))
+                <div class=" links">
+            
+                    @auth
+                        <a class="{{ Request::is('connect') ? 'active' : '' }}" href="{{ url('/home') }}">About</a>
+                        
+                    @else
+                        <a class="{{ Request::is('connect') ? 'active' : '' }}" href="{{ route('login') }}">Login</a>
+                        <a class="{{ Request::is('connect') ? 'active' : '' }}" href="{{ route('register') }}">Register</a>
+                    @endauth
+                    
+                </div>
+            @endif
+        <!--</div>-->
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
+                        @auth
+                        
+                        <div class=" links">
+                        <a class="{{ Request::is('connect') ? 'active' : '' }}" href="{{ url('/home') }}" role="button"   v-pre>
+        </div>
+                                    {{ Auth::user()->name }}
+                                </a>
+                        <!--<li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    
+                                
+                                <!--<a href="{{ url('/home') }}">Home</a>-->
+                                
+                                <!--<a class="dropdown-item" href="{{ url('/home') }}">
+                                        {{ url('/home') }}
+                                </a>-->
+                                <!--<a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>-->
+        </div>
+                    @else
+                   <!-- <li class="{{ Request::is('login') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    <li class="{{ Request::is('register') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>-->
+                    @endauth
+                       
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="flex-center position-ref full-height">
+           <!-- @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                        
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif-->
+
+
+
+
+
+    <!--<div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel"  style="padding: 0%; border: 0%;">
+            <div class="container">
+                <a class="navbar-brand nav nav-justified" href="{{ url('/') }}">
+                    <img src="{{ asset('image/IMMERJ_LOGO_NAVBAR.png') }}" alt="IMMERJ LOGO" width="125" height="35">-->
+                    <!--<img src="{{ config('app.img') }}" alt="IMMERJ LOGO" width="125" height="35"></>-->
+                    <!--{{ config('app.name', 'IMMERJ') }}-->
+                <!--</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+                    <!-- Left Side Of Navbar -->
+                    <!--<ul class="navbar-nav mr-auto">
+
+                    </ul>-->
+
+                    <!-- Right Side Of Navbar -->
+                    <!--<ul class="navbar-nav ml-auto">-->
+                        <!-- Authentication Links -->
+                        <!--@guest
                             <li class="{{ Request::is('login') ? 'active' : '' }}">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
@@ -72,7 +241,7 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav>-->
 
         <main class="py-4">
             @yield('content')
