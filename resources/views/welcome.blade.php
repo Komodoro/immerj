9174cea2c144741377a2f9694266951c6870d51d
rmntavares <ricardo@ricardomnt.com>
2018-05-06 05:57:49 +0100

    mm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
+                background-image: url("image/homepage.jpg");
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
